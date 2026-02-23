aef11ba17ba09d52203ebb236d5a2cf3f872f05e
GovIndLok <97396655+GovIndLok@users.noreply.github.com>
2026-02-24 03:59:00 +0530

    feat: Add `silver_drivers` dbt model and refresh Python package dependencies.

---
+++ f1_dbt/models/silver/silver_drivers.sql
@@ -0,0 +1,53 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+WITH source_drivers AS (
+    SELECT * FROM {{ ref('bronze_drivers') }}
+),
+
+WITH deduplicate_drivers AS (
+    SELECT *,
+    ROW_NUMBER() OVER (
+        PARTITION BY driverId 
+    ) AS rowNum
+    FROM source_drivers
+),
+
+cleaned_drivers AS (
+    SELECT 
+    driverId,
+    driverRef,
+    TRY_CAST(NULLIF(number, '\N') AS INT) AS number,
+    TRY_CAST(NULLIF(code, '\N') AS STRING) AS code,
+    forename,
+    surname,
+    dob,
+    nationality,
+    url
+    FROM deduplicate_drivers
+    WHERE rowNum = 1  --filtering early. Deduplicate
+),
+
+casted_drivers AS (
+    SELECT
+    driverId,
+    driverRef,
+    CAST(number AS INT) AS number,
+    LEFT(CAST(code AS STRING), 3) AS code,
+    forename,
+    surname,
+    CAST(dob AS DATE) AS dob,
+    nationality,
+    url
+    FROM cleaned_drivers
+),
+
+final AS (
+    SELECT *
+    FROM casted_drivers
+)
+
+SELECT * FROM final
