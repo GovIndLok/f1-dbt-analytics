56b0ce975d239b78d090df130c05a390b762084f
GovIndLok <97396655+GovIndLok@users.noreply.github.com>
2026-03-03 05:18:19 +0530

    feat: Add `silver_driver_num` model to track driver number usage over time and update `schema.yml` with its definition and tests.

---
+++ f1_dbt/models/silver/silver_driver_num.sql
@@ -0,0 +1,30 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+WITH race_result as (
+    SELECT
+        result.driverId,
+        race.year as raceYear,
+        result.number as driverNumber,
+        MIN(race.date) as firstDate,
+        MAX(race.date) as lastDate,
+        COUNT(DISTINCT result.raceId) as raceCount
+    FROM {{ ref('bronze_results') }}  result
+    JOIN {{ ref('bronze_races') }}  race
+    on result.raceId = race.raceId
+    WHERE result.number IS NOT NULL
+    GROUP BY result.driverId, raceYear, driverNumber
+)
+
+SELECT 
+    driverId,
+    raceYear,
+    driverNumber, 
+    firstDate,
+    lastDate,
+    raceCount
+FROM race_result
+ORDER BY driverId, raceYear
